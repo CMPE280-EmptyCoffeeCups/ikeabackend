e7ab37eea2437468e2c71d39438e75275ca6bd67
Ekta Sorathia <ektasorathia@Ektas-MacBook-Pro.local>
2017-11-01 20:35:41 -0700

    User added to database

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,46 @@
 var express = require('express');
 var router = express.Router();
-
+var mongo = require("./mongo");
 // Authenticate user and add to DB if needed.
 router.post('/authenticate', function(req, res, next) {
-  console.log(req.body);
-  res.send({'user':'user'});
+  var email = req.body.email;
+  var fname = req.body.given_name;
+  var lname = req.body.family_name;
+  var requestJSON = {"email":email};
+  var resJSON={};
+  mongo.findOne('USER_DETAILS',requestJSON,function(err,searchRes){
+      if(err){
+          console.log(err);
+          res.status(500).send({
+              error: 'There was an error.'
+          });
+      }else{
+
+          if(searchRes==null){
+              var inserJSON={"email":email,"fname":fname,"lname":lname};
+              mongo.insert('USER_DETAILS',inserJSON,function(err,insertRes){
+                  if(err){
+                      console.log("Failed to insert user. "+err);
+                      res.status(500).send({
+                          error: 'There was an error.'
+                      });
+                  }else{
+                      console.log("INSERTING USER");
+                      res.status(200).send({
+                            msg: 'Account created.'
+                      });
+                  }
+              });
+          }else{
+             console.log("User already exists");
+              res.status(200).send({
+                  msg: 'Account created.'
+              });
+          }
+
+      }
+  });
+
 });
 
 module.exports = router;
