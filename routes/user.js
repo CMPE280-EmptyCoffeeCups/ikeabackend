e32657dcbcfb7b90259f58ac6b5d8a82f06457d5
Ekta Sorathia <ektasorathia@Ektas-MacBook-Pro.local>
2017-11-03 00:00:30 -0700

    fixes

--- routes/user.js
+++ routes/user.js
@@ -17,7 +17,7 @@ router.post('/authenticate', function(req, res, next) {
       }else{
 
           if(searchRes===null){
-              var insertJSON={"email":email,"fname":fname,"lname":lname};
+              var insertJSON=req.body;
               mongo.insert('USER_DETAILS',insertJSON,function(err,insertRes){
                   if(err){
                       console.log("Failed to insert user. "+err);
