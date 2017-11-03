c07a9c46ef0adc6fb1a2d90b4ae0b7c76a28eddf
Ekta Sorathia <ektasorathia@Ektas-MacBook-Pro.local>
2017-11-02 19:42:43 -0700

    search user and update user profile API created

--- routes/user.js
+++ routes/user.js
@@ -26,11 +26,13 @@ router.post('/authenticate', function(req, res, next) {
                       });
                   }else{
                       console.log("INSERTING USER");
+
                       res.status(200);
                   }
               });
           }else{
              console.log("User already exists");
+              console.log(searchRes);
               res.status(200);
           }
 
@@ -38,5 +40,60 @@ router.post('/authenticate', function(req, res, next) {
   });
 
 });
+
+router.get('/profile', function(req, res, next) {
+    var email = req.body.email;
+    mongo.findOne('USER_DETAILS',requestJSON,function(err,searchRes){
+        if(err){
+            console.log(err);
+            res.status(500).send({
+                error: 'There was an error.'
+            });
+        }else{
+
+            if(searchRes!=null){
+                console.log(searchRes);
+                //res.status(200).send({"user":searchRes});
+            }else{
+                //res.status(404).send({"message": "User Profile not found."});
+            }
+        }
+    });
+
+});
+
+router.post('/updateProfile',function(req,res,next){
+    var email = req.body.email;
+    var fname = req.body.given_name;
+    var lname = req.body.family_name;
+    var address = req.body.address;
+    var cardnumber = req.body.cardnumber;
+    var mm = req.body.mm;
+    var yy = req.body.yy;
+    var cvv = req.body.cvv;
+    var cardholdername = req.body.holdername;
+    var billingAddr=req.body.billAddr;
+    var paymentDetails={"cardnumber":cardnumber,"mm":mm,"yy":yy,"cvv":cvv,"cardholdername":cardholdername,"billingAddr":billingAddr};
+    var profileJSON = {"fname":fname,"lname":lname,"address":address,"payment":paymentDetails};
+    var queryJSON = {"email":email};
+    mongo.updateOne('USER_DETAILS',queryJSON,profileJSON,function(err,searchRes){
+        if(err){
+            console.log(err);
+            res.status(500).send({
+                error: 'There was an error.'
+            });
+        }else{
+
+            if(searchRes!=null){
+                console.log(searchRes);
+                //res.status(200).send({"user":searchRes});
+            }else{
+                //res.status(404).send({"message": "User Profile not found."});
+            }
+        }
+    });
+});
+
+
 
 module.exports = router;
