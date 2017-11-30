296c0ed8929bbdfb53be737cebc1ac60fffa6d8d
Ekta Sorathia <ektasorathia@Ektas-MacBook-Pro.local>
2017-11-30 15:17:44 -0800

    get product api created

--- routes/items.js
+++ routes/items.js
@@ -24,4 +24,26 @@ router.get('/', function(req, res) {
     });
 });
 
+
+router.get('/product', function(req, res) {
+    console.log(req.query.prod_id);
+    var productid = mongodb.ObjectID(req.query.prod_id);
+    var requestJSON={"_id":productid};
+    mongo.findOne('PRODUCT_DETAILS',requestJSON,function(err,searchRes){
+        if(err){
+            console.log(err);
+            res.status(500).send({
+                error: 'There was an error.'
+            });
+        }else{
+
+            if(searchRes!==null){
+                console.log(searchRes);
+                res.status(200).send({"user":searchRes});
+            }else{
+                res.status(404).send({"message": "No product found."});
+            }
+        }
+    });
+});
 module.exports = router;
